c5be1a73a9dd9b68a2984fe1a6dd3136131ca23b
kielx <kiel66@gmail.com>
2021-07-04 10:21:21 +0200

    Add input and handler for 1 rep max

--- src/App.js
+++ src/App.js
@@ -1,3 +1,21 @@
+import {
+  calculateIncrementsForWeek,
+  calculateBase,
+} from "./helpers/calculateIncrements";
+import React, { useState } from "react";
+
 export default function App() {
-  return <h1>Strength-Hub</h1>;
+  const [oneRepMax, setOneRepMax] = useState(0);
+
+  function handleChange(event) {
+    setOneRepMax(event.target.value);
+  }
+
+  return (
+    <>
+      <h1>Strength-Hub</h1>
+      <input type="number" name="oneRepMax" onChange={handleChange}></input>
+      <p>{calculateIncrementsForWeek(1, calculateBase(oneRepMax))}</p>
+    </>
+  );
 }
