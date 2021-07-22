f5d7b644f90959632e2473ad19985093ed3ef310
kielx <kiel66@gmail.com>
2021-07-22 09:31:57 +0200

    Change main image to next gen format

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import main from "../../assets/images/main.jpg";
+import main from "../../assets/images/main.webp";
 
 export default function Home() {
   return (
